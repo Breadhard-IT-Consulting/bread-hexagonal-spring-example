016ed029792de5ce042d0a30bd680194ece4e399
Javier Ramos Panduro <djavierramos@hotmail.com>
2022-11-26 19:24:14 +0100

    Scaffolding

---
+++ src/main/java/com/breadhard/hexagonalspringexample/HexagonalSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.breadhard.hexagonalspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalSpringExampleApplication.class, args);
+	}
+
+}
